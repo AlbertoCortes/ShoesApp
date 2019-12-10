8ab4d1683595601b633eaa4f2f8667aa1a02098c
AlbertoCortes <alberto.cortesb@softtek.com>
2019-12-10 09:42:13 -0600

    Prueba 1

--- ShoesApp/Program.cs
+++ ShoesApp/Program.cs
@@ -18,5 +18,10 @@ namespace ShoesApp
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
+
+        //  TEST github
+
     }
 }
