efb8879c330547efa71980485e7f1a4959f6d2b0
AlbertoCortes <alberto.cortesb@softtek.com>
2019-12-11 12:34:39 -0600

    ...

---
+++ ShoesAppEntities/Productos.cs
@@ -0,0 +1,22 @@
+﻿namespace ShoesAppEntities
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public int? IdType { get; set; }
+        public int? IdColor { get; set; }
+        public int? IdBrand { get; set; }
+        public int? IdProvider { get; set; }
+        public int? IdCatalog { get; set; }
+        public string Title { get; set; }
+        public string Nombre { get; set; }
+        public string Description { get; set; }
+        public string Observations { get; set; }
+        public decimal? PriceDistributor { get; set; }
+        public decimal PriceClient { get; set; }
+        public decimal PriceMember { get; set; }
+        public bool IsEnabled { get; set; }
+        public string Keywords { get; set; }
+        public System.DateTime DateUpdate { get; set; }
+    }
+}
